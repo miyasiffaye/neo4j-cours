4985160ffe0ba40bc5bc58d810795a3db7156f13
ansoumana <ansoumanacisse@gmail.com>
2025-01-31 15:38:45 +0100

    Projet final 2025: Exemple concret
    (cherry picked from commit aa23423dcc30c3c0a545a8c71cb79f9e7f063b5d)

--- neo4j-procedure-template-5.x/src/main/java/com/sorbonne/CreateNeuron.java
+++ neo4j-procedure-template-5.x/src/main/java/com/sorbonne/CreateNeuron.java
@@ -1,5 +1,6 @@
 package com.sorbonne;
 
+import org.eclipse.jetty.util.Index;
 import org.neo4j.graphdb.*;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
@@ -21,14 +22,14 @@ public class CreateNeuron {
     @Description("")
     public Stream<CreateResult> createNeuron(@Name("id") String id,
                                        @Name("layer") String layer,
-                                       @Name("n_type") String type,
+                                       @Name("type") String type,
                                        @Name("activation_function") String activation_function
     ) {
         try (Transaction tx = db.beginTx()) {
 
           tx.execute("CREATE (n:Neuron {\n" +
                     "id: '" + id + "',\n" +
-                    "layer:'" + layer + "',\n" +
+                    "layer:" + layer + ",\n" +
                     "type: '" + type + "',\n" +
                     "bias: 0.0,\n" +
                     "output: null,\n" +
@@ -44,13 +45,34 @@ public class CreateNeuron {
             return Stream.of(new CreateResult("ko"));
         }
     }
+    @Procedure(name = "nn.createRelationShipsNeuron",mode = Mode.WRITE)
+    @Description("")
+    public Stream<CreateResult> createRelationShipsNeuron(
+            @Name("from_id") String from_id,
+            @Name("to_id") String to_id,
+             @Name("weight") String weight
+    ) {
+        try (Transaction tx = db.beginTx()) {
 
-        public static class CreateResult {
+            tx.execute(
+            "MATCH (n1:Neuron" + "{id:'"+ from_id +"'})\n" +
+            "MATCH (n2:Neuron" + "{id:'"+ to_id +"'})\n" +
+            "CREATE (n1)-[:CONNECTED_TO {weight:" + weight + "}]->(n2)"
+            );
+            tx.commit();
+            return Stream.of(new CreateResult("ok"));
 
-            public final String result;
+        } catch (Exception e) {
 
-            public CreateResult(String result) {
-                this.result = result;
-            }
+            return Stream.of(new CreateResult("ko"));
         }
+    }
+    public static class CreateResult {
+
+        public final String result;
+
+        public CreateResult(String result) {
+            this.result = result;
+        }
+    }
 }
