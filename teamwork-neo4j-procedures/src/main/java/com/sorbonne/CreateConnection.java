86029bfc007f1ac9acab3b83aeb4e6350da6286e
Svetlana Tsuranova <s.tsuranava@gmail.com>
2025-02-16 11:04:18 +0100

    Revert createConnection, createNeuron to Cypher

--- teamwork-neo4j-procedures/src/main/java/com/sorbonne/CreateConnection.java
+++ teamwork-neo4j-procedures/src/main/java/com/sorbonne/CreateConnection.java
@@ -10,25 +10,15 @@ public class CreateConnection {
     public GraphDatabaseService db;
 
     @Procedure(name = "nn.createConnection",mode = Mode.WRITE)
-    @Description("Crée une connexion entre deux neurones avec un poids donné.")
+    @Description("Creates a connection between two neurones.")
     public Stream<CreateResult> createConnection(@Name("from_id") String fromId,
                                                  @Name("to_id") String toId,
                                                  @Name("weight") Double weight) {
         try (Transaction tx = db.beginTx()) {
-            Node neuron1 = tx.findNode(Label.label("Neuron"), "id", fromId);
-            Node neuron2 = tx.findNode(Label.label("Neuron"), "id", toId);
-
-            if (neuron1 == null) {
-                return Stream.of(new CreateResult("Error: Source neuron was not found."));
-            }
-            if (neuron2 == null) {
-                return Stream.of(new CreateResult("Error: Target neuron was not found."));
-            }
-
-            Relationship rel = neuron1.createRelationshipTo(neuron2, RelationshipType.withName("CONNECTED_TO"));
-            rel.setProperty("weight", weight);
+            tx.execute("MATCH (n1:Neuron {id: '" + fromId + "' })\n" +
+                          "MATCH (n2:Neuron {id: '" + toId + "'})\n" +
+                          "CREATE (n1)-[:CONNECTED_TO {weight: " + weight + "}] -> (n2)");
             tx.commit();
-
             return Stream.of(new CreateResult("Success"));
         } catch (Exception e) {
             return Stream.of(new CreateResult("Error: " + e.getMessage()));
@@ -43,3 +33,4 @@ public class CreateConnection {
         }
     }
 }
+
--- teamwork-neo4j-procedures/src/main/java/com/sorbonne/CreateNeuron.java
+++ teamwork-neo4j-procedures/src/main/java/com/sorbonne/CreateNeuron.java
@@ -1,42 +1,52 @@
 package com.sorbonne;
 
 import org.neo4j.graphdb.*;
+import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
 
 import java.util.stream.Stream;
 
+
 public class CreateNeuron {
+    // This gives us a log instance that outputs messages to the
+    // standard log, normally found under `data/log/console.log`
+    @Context
+    public Log log;
     @Context
     public GraphDatabaseService db;
 
     // Exemple de base à lire completer corriger
-    // Pour utiliser call nn.createNeuron("1-2","0","input","softmax")
-    @Procedure(name = "nn.createNeuron",mode = Mode.WRITE)
+    // Pour utiliser call nn.createNeuron("123","0","input","sotfmax")
+    @Procedure(name = "nn.createNeuron", mode = Mode.WRITE)
     @Description("Creates a neuron")
     public Stream<CreateResult> createNeuron(@Name("id") String id,
                                              @Name("layer") Long layer,
                                              @Name("type") String type,
-                                             @Name("activation_function") String activation_function) {
+                                             @Name("activation_function") String activation_function
+    ) {
         try (Transaction tx = db.beginTx()) {
-            Node neuron = tx.createNode(Label.label("Neuron"));
-            neuron.setProperty("id", id);
-            neuron.setProperty("layer", layer);
-            neuron.setProperty("type", type);
-            neuron.setProperty("bias", 0.0);
-            neuron.setProperty("m_bias", 0.0);
-            neuron.setProperty("v_bias", 0.0);
-            if (activation_function != null) {
-                // Input layer doesn't have activation function
-                neuron.setProperty("activation_function", activation_function);
-            }
+
+            tx.execute("CREATE (n:Neuron {\n" +
+                    "id: '" + id + "',\n" +
+                    "layer:" + layer + ",\n" +
+                    "type: '" + type + "',\n" +
+                    "bias: 0.0,\n" +
+                    "output: null,\n" +
+                    "m_bias: 0.0,\n" +
+                    "v_bias: 0.0,\n" +
+                    "activation_function:'" + activation_function + "'\n" +
+                    "})");
             tx.commit();
             return Stream.of(new CreateResult("Success"));
+
         } catch (Exception e) {
-            return Stream.of(new CreateResult("Error: " + e.getMessage()));
+
+            return Stream.of(new CreateResult("Error"));
         }
     }
 
     public static class CreateResult {
+
         public final String result;
 
         public CreateResult(String result) {
@@ -44,3 +54,5 @@ public class CreateNeuron {
         }
     }
 }
+
+
